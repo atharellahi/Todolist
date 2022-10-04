6bc20655821b3ad6731acb5a2889388378a930ed
atharellahi <official.atharellahi@gmail.com>
2022-10-04 17:23:11 +0530

    hi

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+import Lst from "./Lst";
+import './styles/styles.css'
+
+
+const App = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [toDoList, setToDoList] = useState([]);
+
+
+    const onbtnclick = () => {
+
+        if (!inputValue) {
+            setToDoList([...toDoList])
+        }
+        else {
+            setToDoList([...toDoList, inputValue])
+
+            setInputValue('');
+
+        }
+    }
+
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <div className="input">
+                <label for='inputfield'>Add task to the list</label>
+                <input type='text' id="inputfield" className="inputadd" value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+                <button id="addbtn" className="add" onClick={() => onbtnclick()} >&#43;</button>
+            </div>
+            <div className="list">
+                <Lst dolist={toDoList} setdolist={setToDoList}/>
+            </div>
+        </div>
+    )
+}
+export default App;
+
+
+//hakak
---
+++ src/components/Lst.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react"
+
+
+const Lst = ({ dolist , setdolist }) => {
+
+    
+
+    const onbtnclick = (e) => {
+      
+        setdolist(dolist.filter((value,index) => {return index != e.target.id;}))
+    
+    }
+
+   
+
+    const renderedList = dolist.map((item, index) => {
+        return (
+            <div key={index} id={index} className='item'><span>{item}</span> <button id={index} className='dlt' onClick={onbtnclick}>&#128465;</button></div>
+        )
+    })
+    return (
+        <React.Fragment>
+            {renderedList}
+        </React.Fragment>
+    )
+}
+
+export default Lst;
